cdbe8d5ef9b6e646201a9173001f5db0581a9a9a
Salman Shah <sbs.191197@gmail.com>
2018-06-07 05:36:41 +0530

    Import Wishlist Final Script added

--- ia-wishlist-bot/import_wishlist_final.py
+++ ia-wishlist-bot/import_wishlist_final.py
@@ -3,6 +3,8 @@ from olclient.openlibrary import OpenLibrary
 import olclient.common as common
 
 import csv
+import requests
+import json 
 
 # File used in the whole script
 FILE = 'data/wishlist_works_may_2018.csv'
@@ -19,17 +21,34 @@ with open(FILE, "rt") as infile:
     next(reader, None)
     for row in reader:
         # print(row[6])
+
+        # Calls using the Open Library Client
         work = ol.Edition.get(isbn=row[5], oclc=row[4])
         work1 = ol.Edition.get(isbn=row[6])
         
-        if work is None and work1 is None and count != 100:
-            count = count + 1
-            value.append(row)
-            print("Count: "+str(count))
-            # print(row[0])
-        elif count == 100:
-            with open("data/new_wishlist_salman.csv", "w") as outfile:
-                csvwriter = csv.writer(outfile)
-                for out_data in value:
-                    csvwriter.writerow(out_data)
-            exit(0)
+        new_title = row[0].replace(" ", "+")
+        r = requests.get("http://openlibrary.org/search.json?q=title:"+str(new_title))
+        if r.status_code == 200:
+            j = json.loads(r.text)
+        
+            match = False
+            for doc in j['docs']:
+                if doc['title_suggest'].lower() == row[0].lower():
+                    match = True
+
+            if work is None and work1 is None and not match and count != 100:
+                count = count + 1
+                value.append(row)
+                print("Count: "+str(count))
+                print(row[0])
+            elif count == 100:
+                with open("data/new_wishlist_salman_10.csv", "w") as outfile:
+                    csvwriter = csv.writer(outfile)
+                    for out_data in value:
+                        csvwriter.writerow(out_data)
+                exit(0)
+            else:
+                print("No match found")
+                # print(row[0])
+        else:
+            print(str(row[0])+" has been skipped")
