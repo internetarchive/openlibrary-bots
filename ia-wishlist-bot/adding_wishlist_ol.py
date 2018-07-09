0abe0d71be76b6f5c255a9e219255929fad358e6
Salman Shah <sbs.191197@gmail.com>
2018-07-09 18:51:34 +0530

    Pass all current testcases

--- ia-wishlist-bot/adding_wishlist_ol.py
+++ ia-wishlist-bot/adding_wishlist_ol.py
@@ -12,17 +12,21 @@ FILE = 'ia-data/new_wishlist_salman_1000.csv'
 
 class TestWishlistAddBook(unittest.TestCase):
     def test_parse_wishlist_csv_row_to_dict(self):
-        csv = "foo,bar,baz,qux"
-        expected = {"author": "foo", "title": "bar"}
+        csv = ["Larks in a paradise: New Zealand portraits", "['McNeish, James', 'Friedlander, Marti']", "eng", "1974", "16289249", "0002114976", "9780002114974"]
+        # expected = {"title": "foo", "authors": ['bar'], "language": "baz", "date": }
+        expected = {"title": "Larks in a paradise: New Zealand portraits", "authors": [
+            'McNeish, James', 'Friedlander, Marti'], "language": "eng", "date": "1974", "oclc": "16289249", "isbn10": "0002114976", "isbn13": "9780002114974"}
+
         self.assertTrue(parse_wishlist_csv_row_to_dict(csv) == expected)
 
     def test_check_if_author_exists(self):
-        author = {'author_name': 'J. R. Tolkien',
+        author = {'author_name': 'Dan Brown',
                   'author_birth_date': 'XXX', 'author_death_date':'XXX'}  # replace XXX w/ valid date
         # great place to check that our code works if certain dates are missing!
-        expected = 'OL123A'  # TODO: replace 123 w/ valid olid
+        expected = 'OL39307A'  # TODO: replace 123 w/ valid olid
         olid = check_if_author_exists(
             author['author_name'], author['author_birth_date'], author['author_death_date'])
+        print(olid)       
         self.assertTrue(expected == olid)
 
 
@@ -33,22 +37,34 @@ def parse_wishlist_csv_row_to_dict(csv):
         >>> parse_wishlist_csv_row_to_dict("foo,bar,baz,qux")
         { "author": "foo", "title": "bar", ...}
     """
-    book = {"title": csv[0], "author_name": ast.literal_eval(csv[1]), "language": csv[2], "date": csv[3], "oclc": csv[4], "isbn10": csv[5], "isbn13": csv[6]}
+    book = {"title": csv[0], "authors": ast.literal_eval(csv[1]), "language": csv[2], "date": csv[3], "oclc": csv[4], "isbn10": csv[5], "isbn13": csv[6]}
     return book
 
 
-def check_if_author_exists(author_name, author_birth_data, author_death_date):
+def check_if_author_exists(author_name, author_birth_date=None, author_death_date=None):
     # TODO -- search for author or return None (using olclient's existing methods -- we did this already for our last step)
-    author = ol.Author.get_olid_by_name(author_name)
-    return author  # this may be None, that's OK
+    author_olid = ol.Author.get_olid_by_name(author_name)
+    if author_olid == None:
+        return None
+    return author_olid
+
+    # return ol.get(author_olid)  # this may be None, that's OK
 
 
 def process_book(book):
     # make sure we've normalized the author name (e.g. first last?)
-    author = check_if_author_exists(
-        book['author_name'], book['author_birth_date'], book['author_death_date'])
+    author_list = []
+    for author in book.get('authors'):
+        author_obj = check_if_author_exists(author)
+        if author_obj:
+            author_list.append(author_obj)
+        else:
+            author_list.append(author) 
     # TODO: bookcover search, etc
-    # TODO: add book to Open Library vie olclient
+
+
+    # TODO: add book to Open Library via olclient
+
 
 def process_csv(filename):
     """This function takes a csv file which was output from our whatever process created e.g. *new_wishlist_salman_1000.csv* and converts it into a python dictionary
@@ -69,7 +85,9 @@ if __name__ == "__main__":
     # csv_row = sys.argv[1]
     book_data = process_csv(FILE)
 
-    for data in book_data:
-        book = parse_wishlist_csv_row_to_dict(data)
-        process_book(book)
+    # for data in book_data:
+    #     book = parse_wishlist_csv_row_to_dict(data)
+    #     process_book(book)
     # book = parse_wishlist_csv_row_to_dict("foo,bar,baz,qux")
+
+    unittest.main()
