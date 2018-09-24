618bd9591809d95e1b3f5f160ac6ff3f7cf40457
Charles Horn <charles.horn@gmail.com>
2018-09-24 15:12:20 +1200

    add bulkmarc import bot

---
+++ ia-bulkmarc-bot/bulk-import.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+""" 
+    Iterates over an archive.org bulk MARC item, such as OpenLibraries-Trent-MARCs,
+    and imports all records in all of its MARC files to Open Library.
+
+    USAGE: ./bulk-import.py <archive.org item id>
+
+    Logs results to STDOUT
+"""
+
+import internetarchive as ia
+import sys
+from olclient.openlibrary import OpenLibrary
+from collections import namedtuple
+
+LIVE = False # False to test against a local dev OL instance
+BULK_API = '/api/import/ia'
+item = "trent_test_marc"
+item = "OpenLibraries-Trent-MARCs"
+if len(sys.argv) > 1:
+    item = sys.argv[1]
+
+if LIVE:
+    ol = OpenLibrary()
+else:
+    local_dev = 'http://localhost:8080'
+    Credentials = namedtuple('Credentials', ['username', 'password'])
+    c = Credentials('openlibrary@example.com', 'admin123')
+    ol = OpenLibrary(base_url=local_dev, credentials=c)
+
+limit = 0 # if non-zero, a limit to only process this many records from each file, for testing
+count = 0
+
+for f in ia.get_files(item):
+    if f.name.endswith('.mrc'):
+        print('FILENAME: %s' % f.name)
+        offset = 0
+        length = 5 # we only need to get the length of the first record (first 5 bytes), the API will seek to the end.
+
+        while length:
+            identifier = '{}/{}:{}:{}'.format(item, f.name, offset, length)
+            data = {'identifier': identifier, 'bulk_marc': 'true'}
+            r = ol.session.post(ol.base_url + BULK_API, data=data)
+            result = r.json()
+            # log results to stdout
+            print('{}: {} -- {}'.format(identifier, r.status_code, result))
+            offset = result.get('next_record_offset')
+            length = result.get('next_record_length')
+            count += 1
+            if limit and count >= limit:
+                # Stop if a limit has been set, and we are over it.
+                length = False
