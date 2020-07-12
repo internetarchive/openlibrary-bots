4dea49620605eac83081b07ab3189965f71d648c
guyjeangilles <7256157+guyjeangilles@users.noreply.github.com>
2020-07-12 17:10:14 +0000

    Push didn't add the code for some reason

---
+++ isbnbot/find_editions_with_isbns.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ -z $1 ]]
+  then
+    echo "No dump file provided"
+    exit 1
+fi
+if [[ -z $2 ]]
+  then
+    echo "No output file provided"
+    exit 1
+fi
+OL_DUMP=$1
+OUTPUT=$2
+
+zgrep ^/type/edition $OL_DUMP | grep -E '("isbn_10":|"isbn_13":)' | pv | gzip > $OUTPUT
