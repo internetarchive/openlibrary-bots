fa56b84f754ff5adf53f157dd28145cf71edd449
Charles Horn <charles.horn@gmail.com>
2020-07-23 15:24:02 -0700

    add local_id barcode listing with -b and -i

--- ia-bulkmarc-bot/bulk-import.py
+++ ia-bulkmarc-bot/bulk-import.py
@@ -17,7 +17,7 @@ from olclient.openlibrary import OpenLibrary
 
 
 BULK_API = '/api/import/ia'
-
+LOCAL_ID = re.compile(r'\/local_ids\/(\w+)')
 
 def get_marc21_files(item):
     return [f.name for f in ia.get_files(item) if f.name.endswith('.mrc')]
@@ -25,8 +25,9 @@ def get_marc21_files(item):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Bulk MARC importer.')
-    parser.add_argument('item', help='Source item containing MARC records')
+    parser.add_argument('item', help='Source item containing MARC records', nargs='?')
     parser.add_argument('-i', '--info', help='List available MARC21 .mrc files on this item', action='store_true')
+    parser.add_argument('-b', '--barcode', help='Barcoded local_id available for import', nargs='?', const=True, default=False)
     parser.add_argument('-f', '--file', help='Bulk MARC file to import')
     parser.add_argument('-n', '--number', help='Number of records to import', type=int, default=1)
     parser.add_argument('-o', '--offset', help='Offset in BYTES from which to start importing', type=int, default=0)
@@ -36,13 +37,7 @@ if __name__ == '__main__':
     item = args.item
     fname = args.file
     local_testing = args.local
-
-    if args.info:
-        # List MARC21 files, then quit.
-        print('Item %s has the following MARC files:' % item)
-        for f in get_marc21_files(item):
-            print(f)
-        exit()
+    barcode = args.barcode
 
     if local_testing:
         Credentials = namedtuple('Credentials', ['username', 'password'])
@@ -54,7 +49,22 @@ if __name__ == '__main__':
         #ol = OpenLibrary(base_url='https://dev.openlibrary.org')
 
     print('Importing to %s' % ol.base_url)
+    print('ITEM: %s' % item)
     print('FILENAME: %s' % fname)
+
+    if args.info:
+        if barcode is True:
+            # display available local_ids
+            print('Available local_ids to import:')
+            r = ol.session.get(ol.base_url + '/local_ids.json')
+            print(LOCAL_ID.findall(r.json()['body']['value']))
+        if item:
+            # List MARC21 files, then quit.
+            print('Item %s has the following MARC files:' % item)
+            for f in get_marc21_files(item):
+                print(f)
+        exit()
+
 
     limit = args.number  # if non-zero, a limit to only process this many records from each file
     count = 0
