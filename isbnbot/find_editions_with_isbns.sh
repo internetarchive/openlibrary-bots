f1b550bccd6e1b605051905c90c176ccd0c4ccc1
Guyrandy Jean-Gilles <jguyrandy@yahoo.com>
2020-07-26 10:45:40 -0700

    fix regex in grep filter

--- isbnbot/find_editions_with_isbns.sh
+++ isbnbot/find_editions_with_isbns.sh
@@ -15,4 +15,4 @@ fi
 OL_DUMP=$1
 OUTPUT=$2
 
-zgrep ^/type/edition $OL_DUMP | grep -E '"isbn_(10|13)":' |  grep -v -E '"isbn_(10|13)": \["\d+"\]' | pv | gzip > $OUTPUTls
+zgrep ^/type/edition $OL_DUMP | grep -E '"isbn_(10|13)":' |  grep -v -E '"isbn_(10|13)": \["[0-9]+"\]' | pv | gzip > $OUTPUT
