5d8ffc6b1704d4efffa54a74698eab560a9d6353
Michael E. Karpeles (mek) <michael.karpeles@gmail.com>
2020-12-18 02:35:55 -0800

    Refactoring logic
    1. Simplifying main runner flow
    2. Centralizing tweet content
    3. Moving models isbn + book APIs to separate file
    4. Adding test cases

--- twitter-borrowbot/twitterbot.py
+++ twitter-borrowbot/twitterbot.py
@@ -3,151 +3,139 @@ import re
 import time
 import requests
 import tweepy
-import isbnlib
 
+from services import InternetArchive, ISBNFinder
 from dotenv import load_dotenv
+
+ACTIONS = ['read', 'borrow', 'preview']
+READ_OPTIONS = dict(zip(InternetArchive.MODES, ACTIONS))
+BOT_NAME = "@borrowbot"
+STATE_FILE = 'last_seen_id.txt'
+
+
 load_dotenv()
 
-auth = tweepy.OAuthHandler(os.environ.get(
-    'CONSUMER_KEY'), os.environ.get('CONSUMER_SECRET'))
-auth.set_access_token(os.environ.get('ACCESS_TOKEN'),
-                      os.environ.get('ACCESS_TOKEN_SECRET'))
+auth = tweepy.OAuthHandler(
+    os.environ.get('CONSUMER_KEY'),
+    os.environ.get('CONSUMER_SECRET')
+)
+auth.set_access_token(
+    os.environ.get('ACCESS_TOKEN'),
+    os.environ.get('ACCESS_TOKEN_SECRET')
+)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-FILE_NAME = 'last_seen_id.txt'
+
+def get_last_seen_id():
+    with open(STATE_FILE, 'r') as fin:
+        return int(fin.read().strip())
 
 
-def retrieve_last_seen_id(file_name):
-    f_read = open(file_name, 'r')
-    last_seen_id = int(f_read.read().strip())
-    f_read.close()
-    return last_seen_id
+def set_last_seen_id(mention):
+    with open(STATE_FILE, 'w') as fout:
+        fout.write(str(mention.id))
 
 
-def store_last_seen_id(last_seen_id, file_name):
-    f_write = open(file_name, 'w')
-    f_write.write(str(last_seen_id))
-    f_write.close()
-    return
+def get_parent_tweet_of(mention):
+    if mention.in_reply_to_status_id:
+        return api.get_status(
+            mention.in_reply_to_status_id,
+            tweet_mode="extended")
+    return []
 
-def check_tweet(tweet, parent=False):
-    if parent:
-        print("In parent")
-        print(tweet.full_text)
-        print(tweet.in_reply_to_status_id)
-        if tweet.in_reply_to_status_id:
-            tweet = api.get_status(tweet.in_reply_to_status_id, tweet_mode="extended")
-            print(tweet.full_text)
-        else:
-            return []
-    
-    text = tweet.full_text
-    words = text.split()
-    isbnlike = isbnlib.get_isbnlike(text, level='normal')
 
-    print(isbnlike)
-    print(words)
+def get_latest_mentions(since=None):
+    since = since or get_last_seen_id(FILE_NAME)
+    try:
+        return api.mentions_timeline(since, tweet_mode="extended")
+    except Exception as e:
+        print("Exception: %s" % e)
+        return None
 
-    for word in words:
-        if word.startswith("http") or word.startswith("https"):
-            print(word)
-            resp = requests.head(word)
-            print(resp.headers["Location"])
-            if "amazon" in resp.headers["Location"] and "/dp/" in resp.headers["Location"]:
-                amazon_text = isbnlib.get_isbnlike(
-                    resp.headers["Location"], level='normal')
-                amazon_text = list(dict.fromkeys(amazon_text))
-                for item in amazon_text:
-                    if isbnlib.is_isbn10(item) or isbnlib.is_isbn13(item):
-                        isbnlike.append(item)
 
-    print(isbnlike)
+class Tweet:
 
-    return isbnlike
+    @staticmethod
+    def _tweet(mention, message, debug=False):
+        msg = "Hi 👋 @%s %s" % (mention.user.screen_name, message)
+        print(msg)
+        if not debug:
+            api.update_status(
+                msg,
+                in_reply_to_status_id=mention.id,
+                auto_populate_reply_metadata=True
+            )
 
-    
+    @classmethod
+    def edition_available(cls, mention, edition):
+        action = READ_OPTIONS[edition.availability]
+        print('Replying: Edition %sable' % action)
+        cls._tweet(
+            mention,
+            "you're in luck. " +
+            "This book appears to be %sable " % action +
+            "on @openlibrary: " +
+            "%s/isbn/%s" % (InternetArchive.OL_URL, edition.isbn)
+        )
+
+    @classmethod
+    def work_available(cls, mention, work):
+        cls._tweet(
+            mention,
+            "this exact edition doesn't appear to be available, "
+            "however it seems a similar edition may be: " +
+            "https://openlibrary.org/work/" + work.get('openlibrary_work')
+        )
+
+    @classmethod
+    def edition_unavailable(cls, mention, edition):
+        cls._tweet(
+            mention,
+            "this book doesn't appear to have a readable option yet, " +
+            "however you can still add it to your " +
+            "Want To Read list here: " +
+            "%s/isbn/%s" % (InternetArchive.OL_URL, edition.isbn)
+        )
+
+    @classmethod
+    def edition_not_found(cls, mention):
+        print('Replying: Book Not found')
+        cls._tweet(
+            mention,
+            "sorry, I was unable to spot any books! " +
+            "Learn more about how I work here: " +
+            "https://github.com/internetarchive/openlibrary-bots"
+        )
+
 
 def reply_to_tweets():
-    last_seen_id = retrieve_last_seen_id(FILE_NAME)
-
-    try:
-        mentions = api.mentions_timeline(last_seen_id, tweet_mode="extended")
-    except:
-        print("Exception")
-        return
-
+    mentions = get_latest_mentions()
     for mention in reversed(mentions):
         print(str(mention.id) + '- ' + mention.full_text)
-        last_seen_id = mention.id
-        store_last_seen_id(last_seen_id, FILE_NAME)
-        isbnlike = check_tweet(mention, False)
-        if len(isbnlike) == 0:
-            isbnlike = check_tweet(mention, True)
-        if len(isbnlike) == 0:
-            print('Responding back ...')
-            print("sorry, we couldn't find a book ID to look for, learn how I work here: <github README url>")
-            try:
-                api.update_status('Hi 👋 @' + mention.user.screen_name + " Sorry, we couldn't find a book ID to look for, learn how I work here: https://github.com/internetarchive/openlibrary-bots", in_reply_to_status_id=mention.id, auto_populate_reply_metadata=True)
-            except: 
-                pass
-            return
-        for isbn in isbnlike:
-            isbn = isbnlib.canonical(isbn)
-            reply_text = ""
-            if isbnlib.is_isbn10(isbn) or isbnlib.is_isbn13(isbn):
-                try:
-                    resp = requests.get("http://openlibrary.org/isbn/"+isbn+".json").json()
-                except:
-                    print("Error in response continuing")
-                    continue
-                if resp.__contains__("ocaid"):
-                    try:
-                        resp_archive = requests.get("https://archive.org/services/loans/loan/?&action=availability&identifier="+resp["ocaid"]).json()
-                    except:
-                        print("Error in response continuing")
-                        continue
-                    if resp_archive and resp_archive['lending_status']['is_readable']:
-                        reply_text = '@' + mention.user.screen_name + " you're in luck. This book appears to be available to read for free from @openlibrary: https://openlibrary.org/isbn/" + isbn
-                    elif resp_archive and resp_archive['lending_status']['is_lendable']:
-                        reply_text = '@' + mention.user.screen_name + " you're in luck. This book appears to be available to borrow for free from @openlibrary: https://openlibrary.org/isbn/" + isbn
-                    elif resp_archive and resp_archive['lending_status']['is_printdisabled']:
-                        reply_text = '@' + mention.user.screen_name + " you're in luck. This book appears to be available to preview for free from @openlibrary: https://openlibrary.org/isbn/" + isbn
-                    else:
-                        reply_text = '@' + mention.user.screen_name + " This title doesn't appear to have a free read option yet, however you can add it to your Want To Read list here: https://openlibrary.org/isbn/" + isbn
-                else:
-                    try:
-                        resp_advanced = requests.get("https://archive.org/advancedsearch.php?q=openlibrary_work:"+resp["works"][0]['key'].split("/")[-1]+"&fl[]=identifier&sort[]=&sort[]=&sort[]=&rows=50&page=1&output=json").json()
-                    except:
-                        print("Error in response continuing")
-                        continue
-                    if resp_advanced and resp_advanced["response"]["numFound"] > 1:
-                        reply_text = '@' + mention.user.screen_name + " This edition doesn't appear to be available, however I've identified "+str(resp_advanced["response"]["numFound"])+" other editions which may be available here -> https://openlibrary.org" + resp["works"][0]['key']
-                    else:
-                        reply_text = '@' + mention.user.screen_name + " This title doesn't appear to have a free read option yet, however you can add it to your Want To Read list here: https://openlibrary.org/isbn/" + isbn
+        set_last_seen_id(mention.id)
 
-                print('Responding back ...')
-                print(reply_text)
-                try:
-                    api.update_status('Hi 👋 ' + reply_text, in_reply_to_status_id=mention.id, auto_populate_reply_metadata=True)
-                except:
-                    pass
-        # -------------------- To get all replies for the tweet --------------------------------
-        # replies=[]
-        # print("Replies: \n\n")
-        # for tweet in tweepy.Cursor(api.search,q='to:'+'@' + mention.user.screen_name,result_type='recent',timeout=999999).items(1000):
-        #     if hasattr(tweet, 'in_reply_to_status_id_str'):
-        #         # print("has attributed:",str(tweet.in_reply_to_status_id_str))
-        #         # print(mention.id)
-        #         if str(tweet.in_reply_to_status_id_str) == str(mention.id):
-        #             # print("reply appended")
-        #             text = tweet.text
-        #             words = text.split()
-        #             print(words)
-        #             replies.append(tweet.text)
-        # for elements in replies:
-        #     print("Replies :",elements)
-        # -------------------------------------------------------------------------------------
+        if BOT_NAME in mention.full_text:
+            isbns = ISBNFinder.find_isbns(mention.full_text)
+            if not isbns:
+                # fetch tweet's parent (TODO: or siblings) & check for isbns
+                mention = get_parent_tweet_of(mention)
+                isbns = ISBNFinder.find_isbns(mention.full_text)
 
-while True:
-    reply_to_tweets()
-    time.sleep(15)
+            for isbn in isbns:
+                edition = InternetArchive.get_edition(isbn)
+                if edition and edition.availability:
+                    return Tweet.edition_available(mention, edition)
+
+                work = InternetArchive.find_available_work(book)
+                if work:
+                    return Tweet.work_available(mention, work)
+
+            if isbns:
+                return Tweet.edition_unavailable(mention, edition)
+    return Tweet.edition_not_found(mention)
+
+
+if __name__ == "__main__":
+    while True:
+        reply_to_tweets()
+        time.sleep(15)
