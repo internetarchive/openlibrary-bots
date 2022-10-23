f391ebedf95dfab7972faa67eefa8df36e4b73b8
Christian Clauss <cclauss@me.com>
2022-10-23 15:11:20 +0200

    Update CommaTheBot.py

--- CommaTheBot/CommaTheBot.py
+++ CommaTheBot/CommaTheBot.py
@@ -6,24 +6,28 @@ import copy
 import gzip
 import re
 
+
 class CommaTheBotJob(AbstractBotJob):
     pattern = None
 
     def __init__(self):
         super().__init__()
+        # fmt: off
         articles = [
-            '[Tt]he',
-            '[Dd]er', '[Dd]ie', '[Dd]as',
-            '[Ll]e', '[Ll]a', '[Ee]l',
-            '[Ll]os', '[Ll]as', '[Ll]es',
+            "[Tt]he",
+            "[Dd]er", "[Dd]ie", "[Dd]as",
+            "[Ll]e", "[Ll]a", "[Ee]l",
+            "[Ll]os", "[Ll]as", "[Ll]es",
         ]
+        # fmt: on
 
         self.pattern = re.compile(rf"^([\w ,]+), ?({'|'.join(articles)})$")
 
     def needs_fixing(self, edition_title: str) -> bool:
-        if edition_title is None: return False # no title given
+        if edition_title is None:
+            return False  # no title given
 
-        return True if(self.pattern.search(edition_title)) else False
+        return True if (self.pattern.search(edition_title)) else False
 
     def fix_title(self, edition_title: str) -> str:
         match = self.pattern.search(edition_title)
@@ -33,20 +37,24 @@ class CommaTheBotJob(AbstractBotJob):
     def run(self) -> None:
         self.dry_run_declaration()
 
-        comment = 'foo, the -> the foo'
-        with gzip.open(self.args.file, 'rb') as file:
+        comment = "foo, the -> the foo"
+        with gzip.open(self.args.file, "rb") as file:
             for row in file:
                 row, json_data = self.process_row(row)
-                if not self.needs_fixing(json_data.get('title')): # .get() to avoid KeyError
+                if not self.needs_fixing(
+                    json_data.get("title")
+                ):  # .get() to avoid KeyError
                     continue
 
                 # the database may have changed since the dump was created, so call the OpenLibrary API and check again
-                olid = json_data['key'].split('/')[-1]
-                isEdition = json_data['type']['key'] == '/type/edition'
-                book = self.ol.Edition.get(olid) if isEdition else self.ol.Work.get(olid)
+                olid = json_data["key"].split("/")[-1]
+                isEdition = json_data["type"]["key"] == "/type/edition"
+                book = (
+                    self.ol.Edition.get(olid) if isEdition else self.ol.Work.get(olid)
+                )
 
-                if not book.type['key'] in ('/type/edition', '/type/work' ):
-                    continue # skip deleted books
+                if not book.type["key"] in ("/type/edition", "/type/work"):
+                    continue  # skip deleted books
                 if not self.needs_fixing(book.title):
                     continue
 
