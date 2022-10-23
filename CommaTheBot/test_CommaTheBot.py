9b67d585f5927774b429756cd843d0f85680acc6
Christian Clauss <cclauss@me.com>
2022-10-23 15:13:48 +0200

    Update test_CommaTheBot.py

--- CommaTheBot/test_CommaTheBot.py
+++ CommaTheBot/test_CommaTheBot.py
@@ -3,27 +3,28 @@ from CommaTheBot import CommaTheBotJob
 
 commaTheBot = CommaTheBotJob()
 
+
 class TestCommaTheBot(unittest.TestCase):
     def test_no_title(self):
         title = ""
         needs_fixing = commaTheBot.needs_fixing(title)
-        assert needs_fixing == False
+        assert needs_fixing is False
 
     def test_no_fixing(self):
         title = "The Foo"
         needs_fixing = commaTheBot.needs_fixing(title)
-        assert needs_fixing == False
-    
+        assert needs_fixing is False
+
     def test_needs_fixing(self):
         title = "Foo, The"
         needs_fixing = commaTheBot.needs_fixing(title)
-        assert needs_fixing == True
-    
+        assert needs_fixing is True
+
     def test_fix(self):
         title = "Foo, The"
         fixed_title = commaTheBot.fix_title(title)
         assert fixed_title == "The Foo"
-    
+
     def test_pattern_match(self):
         # case of article is kept, with and without space
         title = "Foo, The"
@@ -41,22 +42,22 @@ class TestCommaTheBot(unittest.TestCase):
 
     def test_pattern_no_match(self):
         title = "Foo, The blah"
-        assert commaTheBot.needs_fixing(title) == False
+        assert commaTheBot.needs_fixing(title) is False
 
         # no title
         title = ", The"
-        assert commaTheBot.needs_fixing(title) == False
+        assert commaTheBot.needs_fixing(title) is False
         title = "The"
-        assert commaTheBot.needs_fixing(title) == False
+        assert commaTheBot.needs_fixing(title) is False
 
         # article not at end of line
         title = "Foo, The "
-        assert commaTheBot.needs_fixing(title) == False
+        assert commaTheBot.needs_fixing(title) is False
 
         # maybe support for these should be added
         title = "Foo (bar), The"
-        assert commaTheBot.needs_fixing(title) == False
+        assert commaTheBot.needs_fixing(title) is False
         title = "Foo., The"
-        assert commaTheBot.needs_fixing(title) == False
+        assert commaTheBot.needs_fixing(title) is False
         title = "foo-bar, The"
-        assert commaTheBot.needs_fixing(title) == False
+        assert commaTheBot.needs_fixing(title) is False
