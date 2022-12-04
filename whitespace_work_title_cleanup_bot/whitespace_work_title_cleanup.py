503b7b55ab55ae926b72c3df360e577c56384cee
Jim Man <58356745+jimman2003@users.noreply.github.com>
2022-12-04 14:16:45 +0200

    Add surrounding whitespace work title cleanup bot (#108)
    * Add whitespace_work_title_cleanup
    
    * Add whitespace_work README.md
    
    * Format using black
    
    * Fixed oversights
    
    * change OpenLibrary to Open Library
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Make python3 explicit
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Split comment
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Better error handling
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Convert list to a tuple
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Fix keyerror for malformed titles
    
    * Bump requests from 2.25.1 to 2.26.0
    
    Bumps [requests](https://github.com/psf/requests) from 2.25.1 to 2.26.0.
    - [Release notes](https://github.com/psf/requests/releases)
    - [Changelog](https://github.com/psf/requests/blob/master/HISTORY.md)
    - [Commits](https://github.com/psf/requests/compare/v2.25.1...v2.26.0)
    
    ---
    updated-dependencies:
    - dependency-name: requests
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Christian Clauss <cclauss@me.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ whitespace_work_title_cleanup_bot/whitespace_work_title_cleanup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import copy
+import gzip
+
+from olclient.bots import AbstractBotJob
+
+
+class TrimTitleJob(AbstractBotJob):
+    @staticmethod
+    def needs_trim(work_title: str,) -> bool:  
+        """Returns True if Edition title needs to have whitespace removed. Return false otherwise"""
+        return work_title.strip() != work_title
+
+    def run(self) -> None:  # overwrite the AbstractBotJob run method
+        """Strip leading and trailing whitespace from edition titles"""
+        self.dry_run_declaration()
+
+        comment = "Trim Whitespace"
+        with gzip.open(self.args.file, "rb") as fin:
+            for row in fin:
+                # extract info from the dump file and check it
+                row, json_data = self.process_row(row)
+                if json_data["type"]["key"] != "/type/work":
+                    continue  # this handles full dump (instead for work dump)
+                if not self.needs_trim(json_data.get("title","")):
+                    continue
+
+                # the database may have changed since the dump was created, so call the
+                # OpenLibrary API and check again
+                olid = json_data["key"].split("/")[-1]
+                work = self.ol.Work.get(olid)
+                if work.type["key"] != "/type/work":
+                    continue  # skip deleted editions
+                if not self.needs_trim(work.title):
+                    continue
+
+                # this edition needs editing, so fix it
+                old_title = copy.deepcopy(work.title)
+                work.title = work.title.strip()
+                self.logger.info("|".join((olid, old_title, work.title)))  # log the modifications
+                self.save(lambda: work.save(comment=comment))
+
+
+if __name__ == "__main__":
+    job = TrimTitleJob()
+
+    try:
+        job.run()
+    except Exception:
+        job.logger.exception("whitespace_work_title_cleanup failed")
+        raise
