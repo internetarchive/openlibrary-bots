6d0e9316185d5b729d4e88d036150501ab5a4338
Guyrandy Jean-Gilles <gjgilles@tutanota.com>
2022-12-26 23:02:40 -0500

    add tests for cover_updater.py

---
+++ tests/coverbot/test_cover_updater.py
@@ -0,0 +1,16 @@
+import random
+import unittest
+
+from coverbot.cover_updater import AddInternetArchiveCoverJob as job
+
+
+class AddInternetArchiveCoverJob(unittest.TestCase):
+
+    def test_valid_covers(self):
+        self.assertFalse(job.valid_covers([]))
+        self.assertFalse(job.valid_covers([-1]))
+        self.assertFalse(job.valid_covers([None]))
+        self.assertFalse(job.valid_covers([-1, None]))
+        self.assertFalse(job.valid_covers([None, -1]))
+        self.assertTrue(job.valid_covers([random.randint(1000000, 9999999)]))
+        self.assertTrue(job.valid_covers([random.randint(1000000, 9999999), random.randint(1000000, 9999999)]))
