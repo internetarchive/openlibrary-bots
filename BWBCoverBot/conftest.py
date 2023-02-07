3a02b635733f3e04eb0002fce71685163cdb2b5c
Scott Barnes <scottreidbarnes@gmail.com>
2023-02-06 23:56:40 -0800

    Make bwbcoverbot use cover info from ol dumps (#258)
    * BWBCoverBot: add type hints, give more useful return values, and improve tests.
    
    * Add tests, move call to OpenLibrary() out of global scope to facilitate testing, and add a README.
    
    * BWBCoverBot: Add type hints, give more useful return values, and expand tests.

---
+++ BWBCoverBot/conftest.py
@@ -0,0 +1,33 @@
+from collections.abc import Iterator
+
+import pytest
+from main import EditionCoverData
+from olclient import OpenLibrary, config
+from requests_mock.mocker import Mocker as RequestsMock
+from sqlmodel import Session, SQLModel, create_engine
+
+
+@pytest.fixture(scope="session")
+def get_db() -> Iterator[Session]:
+    engine = create_engine("sqlite:///:memory:", echo=False)
+    SQLModel.metadata.create_all(engine)
+    db_session = Session(engine)
+
+    # Add an ISBN as already seeded into the database.
+    db_session.bulk_save_objects([EditionCoverData(isbn_13="111", cover_exists=True)])
+    db_session.commit()
+
+    yield db_session
+
+
+@pytest.fixture()
+def get_ol(requests_mock: RequestsMock) -> Iterator[OpenLibrary]:
+    requests_mock.post(
+        "https://openlibrary.org/account/login",
+        cookies={
+            "session": "/people/testbot%2C2023-01-20T02%3A26%3A33%2C4819b%246atest;Path=/"
+        },
+    )
+    yield OpenLibrary(
+        credentials=config.Credentials(access="access_key", secret="secret_key")
+    )
