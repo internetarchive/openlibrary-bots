3307a21f6e4e7b16875b1050a3b4208936cf0d20
Guyrandy Jean-Gilles <gjgilles@tutanota.com>
2023-02-16 09:33:22 -0800

    formatting

--- tests/coverbot/test_cover_updater.py
+++ tests/coverbot/test_cover_updater.py
@@ -1,6 +1,5 @@
-import random
 import unittest
-
+from random import randint
 from coverbot.cover_updater import AddInternetArchiveCoverJob as job
 
 
@@ -11,9 +10,6 @@ class AddInternetArchiveCoverJob(unittest.TestCase):
         self.assertFalse(job.valid_covers([None]))
         self.assertFalse(job.valid_covers([-1, None]))
         self.assertFalse(job.valid_covers([None, -1]))
-        self.assertTrue(job.valid_covers([random.randint(1000000, 9999999)]))
-        self.assertTrue(
-            job.valid_covers(
-                [random.randint(1000000, 9999999), random.randint(1000000, 9999999)]
-            )
-        )
+        self.assertTrue(job.valid_covers([randint(1000000, 9999999)]))
+        rand_covers = [randint(1000000, 9999999), randint(1000000, 9999999)]
+        self.assertTrue(job.valid_covers(rand_covers))
